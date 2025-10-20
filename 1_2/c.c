0376caa1dd87c48b4bd1e2d73805c7a6558d6dda
YSqQrH0zurvGmYfF <104148785+YSqQrH0zurvGmYfF@users.noreply.github.com>
2025-10-20 07:09:07 +0000

    Create c.c

---
+++ 1_2/c.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+void* mythread(void* arg) {
+    return (void*) "hello world";
+}
+
+int main(void) {
+    pthread_t tid;
+    int err;
+
+    err = pthread_create(&tid, NULL, mythread, NULL);
+    if (err != SUCCESS) {
+        fprintf(stderr, "Failed to create thread : %s\n", strerror(err));
+        return EXIT_FAILURE;
+    }
+
+    char* thread_result;
+    err = pthread_join(tid, (void**) &thread_result);
+    if (err != SUCCESS) {
+        fprintf(stderr, "Failed to join thread : %s\n", strerror(err));
+        return EXIT_FAILURE;
+    }
+    
+    puts(thread_result);
+    return EXIT_SUCCESS;
+}
+
